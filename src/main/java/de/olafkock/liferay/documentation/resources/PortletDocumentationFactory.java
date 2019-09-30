b1365ac68c93dc1e2733e6203327c4760be7677f
Olaf Kock <olaf.kock+git@liferay.com>
2019-09-30 14:13:03 +0200

    Read configuration from server by default

--- src/main/java/de/olafkock/liferay/documentation/resources/PortletDocumentationFactory.java
+++ src/main/java/de/olafkock/liferay/documentation/resources/PortletDocumentationFactory.java
@@ -1,8 +1,13 @@
 package de.olafkock.liferay.documentation.resources;
 
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.HttpUtil;
+import com.liferay.portal.kernel.util.ReleaseInfo;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
@@ -15,7 +20,7 @@ import java.util.Map;
  *
  */
 public class PortletDocumentationFactory {
-
+	public static final Log log = LogFactoryUtil.getLog(PortletDocumentationFactory.class);
 	/**
 	 * @return null if infrastructure isn't ready yet, e.g. JSON deserializing fails
 	 * for any reason (typically because our bundle may be started before Liferay's 
@@ -26,22 +31,33 @@ public class PortletDocumentationFactory {
 		
 		HashMap<String, PortletDocumentation> result = new HashMap<String, PortletDocumentation>();
 		try {
-			InputStream configuration = PortletDocumentationFactory.class.getResourceAsStream("/content.json");
-			InputStream bis = configuration;
-			ByteArrayOutputStream buf = new ByteArrayOutputStream();
-			int conf_read = bis.read();
-			while(conf_read != -1) {
-			    buf.write((byte) conf_read);
-			    conf_read = bis.read();
+			String json = null;
+			try {
+				String location = "https://www.olafkock.de/liferay/controlpaneldocumentation/content.json?v=" + ReleaseInfo.getVersion();
+				json = HttpUtil.URLtoString(location);
+				log.info("read " + json.length() + " characters config from " + location);
+			} catch (IOException e) {
+				log.error(e);
 			}
-			String json = buf.toString("UTF-8");
+			if(json == null || json.length() < 200) {
+				InputStream configuration = PortletDocumentationFactory.class.getResourceAsStream("/content.json");
+				InputStream bis = configuration;
+				ByteArrayOutputStream buf = new ByteArrayOutputStream();
+				int conf_read = bis.read();
+				while(conf_read != -1) {
+				    buf.write((byte) conf_read);
+				    conf_read = bis.read();
+				}
+				json = buf.toString("UTF-8");
+				log.info("read " + json.length() + " characters config from embedded file");
+			}
 			PortletDocumentation[] docs;
 			docs = JSONFactoryUtil.looseDeserialize(json, PortletDocumentation[].class);
 			for (PortletDocumentation doc : docs) {
 				result.put(doc.portletId, doc);
 			}
 		} catch (Exception e) {
-			e.printStackTrace();
+			log.error(e);
 		}
 		return result;
 	}
